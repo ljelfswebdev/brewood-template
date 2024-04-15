e4d72e5470cb7b6f1d50a6a19ca2e5ab646dfbe5
Lewis <lewis@westmidlandsmedia.co.uk>
2024-04-15 19:03:15 +0100

    add password

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,6 @@ export default function RootLayout({ children }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    // Check if user is already logged in using sessionStorage
     const loggedInStatus = sessionStorage.getItem('isLoggedIn');
     const loginTimestamp = sessionStorage.getItem('loginTimestamp');
 
@@ -18,43 +17,41 @@ export default function RootLayout({ children }) {
       const today = new Date();
       const lastLoginDate = new Date(loginTimestamp);
 
-      // Check if last login was within the last day
       if (today - lastLoginDate <= 24 * 60 * 60 * 1000) {
         setIsLoggedIn(true);
       } else {
-        // If last login was more than a day ago, clear sessionStorage
+  
         sessionStorage.clear();
       }
     }
 
-    // if (!isLoggedIn) {
-    //   // If user is not logged in or login is expired, prompt for login
-    //   const login = () => {
-    //     const envUsername = process.env.NEXT_PUBLIC_USERNAME; 
-    //     const envPassword = process.env.NEXT_PUBLIC_PASSWORD; 
+    if (!isLoggedIn) {
+      const login = () => {
+        const envUsername = process.env.NEXT_PUBLIC_USERNAME; 
+        const envPassword = process.env.NEXT_PUBLIC_PASSWORD; 
 
-    //     let isLoggedIn = false;
+        let isLoggedIn = false;
 
-    //     while (!isLoggedIn) {
-    //       const username = prompt("Enter your username:");
-    //       const password = prompt("Enter your password:");
+        while (!isLoggedIn) {
+          const username = prompt("Enter your username:");
+          const password = prompt("Enter your password:");
 
-    //       if (username === envUsername && password === envPassword) {
-    //         alert("Login successful!");
-    //         isLoggedIn = true;
-    //       } else {
-    //         alert("Invalid username or password. Please try again.");
-    //       }
-    //     }
+          if (username === envUsername && password === envPassword) {
+            alert("Login successful!");
+            isLoggedIn = true;
+          } else {
+            alert("Invalid username or password. Please try again.");
+          }
+        }
 
-    //     // Store login state and timestamp in sessionStorage
-    //     sessionStorage.setItem('isLoggedIn', isLoggedIn);
-    //     sessionStorage.setItem('loginTimestamp', new Date());
-    //     setIsLoggedIn(isLoggedIn);
-    //   };
+        // Store login state and timestamp in sessionStorage
+        sessionStorage.setItem('isLoggedIn', isLoggedIn);
+        sessionStorage.setItem('loginTimestamp', new Date());
+        setIsLoggedIn(isLoggedIn);
+      };
 
-    //   login();
-    // }
+      login();
+    }
   }, []);
 
   return (
